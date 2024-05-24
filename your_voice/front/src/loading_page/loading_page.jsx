83aa07a34373bb3e48b4c23490ccbdb742eb139d
Soojong Kim <sujjong456@gmail.com>
2024-05-24 16:43:40 +0900

    0524

---
+++ your_voice/front/src/loading_page/loading_page.jsx
@@ -0,0 +1,10 @@
+import '../loading_page/loading_page.css'
+
+function loading_page() {
+  return (
+    <div className='loading_box'>
+      <h1>hi</h1>
+    </div>
+  )
+}
+export default loading_page;
