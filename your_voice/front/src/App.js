382c864cef5e33ff0da19757b9e9129a1bb1055b
Marinajwb <byun8688@gmail.com>
2024-05-24 17:47:55 +0900

    0524Marinajwb

--- your_voice/front/src/App.js
+++ your_voice/front/src/App.js
@@ -1,9 +1,11 @@
 import Loading_page from '../src/loading_page/loading_page'
+import MainPg from '../src/MainPg/MainPg.jsx'
 
 function App() {
   return (
     <div>
       < Loading_page />
+      <MainPg />
     </div>
   );
 }
