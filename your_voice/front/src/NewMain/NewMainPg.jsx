aa1061734ba0e1b92eaf0943ed131e0dcaa2e4e2
Marinajwb <byun8688@gmail.com>
2024-05-28 15:27:37 +0900

    호호 newMainPage

--- your_voice/front/src/NewMain/NewMainPg.jsx
+++ your_voice/front/src/NewMain/NewMainPg.jsx
@@ -1,28 +1,29 @@
 import React, { useState } from 'react';
 import './NewMainPg.css';
-import CoughUd from '../upload2/coughUd.jsx'
+import CoughUd from '../upload2/coughUd.jsx';
 import Covidbtn from '../Button/Covidbtn.jsx';
-function NewMainPg() {
+import doctor from '../NewMain/doctor.jpg'
+import MenuBar from '../Route/menu.jsx'
 
+function NewMainPg() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
     };
 
-
-    
-    
-
     return (
-        <div className="main-container">
-           
-            <div className="main-box">
-             
-                <div className='voice'>
-                    <h2 className='voiceDsEg'>COVID-19</h2>
-                    <p className="expln">AI가 당신의 기침소리를 분석하여</p>
-                    <p className="expln">COVID-19를 진단합니다.</p>
+       
+        <div className="mainPG_box">
+             <MenuBar/ >
+            <div className='voice'>
+                <div className='MainImg'>
+                    <img src={doctor} alt='의사' />
+                </div>
+                <div className='voiceContent'>
+                    <h2 className='voiceDsEg'>Voice disorder</h2>
+                    <p className="expln">AI가 당신의 음성을 분석하여</p>
+                    <p className="expln">음성질환 유무를 진단합니다.</p>
                     <Covidbtn />
                 </div>
             </div>
--- your_voice/front/src/Route/route.jsx
+++ your_voice/front/src/Route/route.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import MainPg from '../MainPg/MainPg.jsx';
 import LoadingPage from '../loading_page/loading_page.jsx';
 import Diagnosis_page from '../diagnosis_page/diagnosis_page.jsx'
 import CoughUd from '../upload2/coughUd.jsx'
@@ -11,12 +10,12 @@ const RoutesComponent = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<MainPg />} />
+                <Route path="/" element={<NewMainPg />} />
                 <Route path="/loading_page" element={<LoadingPage />} />
                 <Route path='/diagnosis_page' element={<Diagnosis_page />} />
                 <Route path="/coughUd" element={<CoughUd />} />
                 <Route path="/NewMainPg" element={<NewMainPg />} />
-                <Route path="/my_page" element={<MyPage />} />
+             
             </Routes>
         </Router>
     );
