263515ae78261672a569da533f167be647f3d93d
Soojong Kim <sujjong456@gmail.com>
2024-05-29 14:12:02 +0900

    0529

--- your_voice/front/src/diagnosis_page/diagnosis_page.jsx
+++ your_voice/front/src/diagnosis_page/diagnosis_page.jsx
@@ -3,7 +3,7 @@ import { useLocation } from 'react-router-dom';
 import './diagnosis_page.css';
 import MenuBar from '../Route/menu';
 
-function DiagnosisPage() {
+const DiagnosisPage = () => {
   const location = useLocation();
   const { analysisResult } = location.state;
 
@@ -11,18 +11,15 @@ function DiagnosisPage() {
     <div className='diagnosis_page'>
       <MenuBar />
       <div className='diagnosis_head'>
-        <h1>{analysisResult.title}</h1>
+        <h1>{analysisResult.description}</h1>
       </div>
       <div className='diagnosis_body_container'>
-        <div className='diagnosis_img'>
-          <img className='Sample1' src={analysisResult.image_url} alt="Analysis Result" />
-        </div>
         <div className='diagnosis_body'>
           <h3>{analysisResult.description}</h3>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default DiagnosisPage;
