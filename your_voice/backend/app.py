e111822444122afe8cc40ceda97bdedba4bf023c
minjae <sapoon11@gmail.com>
2024-06-12 14:53:02 +0900

    오후커밋

--- your_voice/backend/app.py
+++ your_voice/backend/app.py
@@ -24,4 +24,8 @@ app.register_blueprint(coughUpload_bp)
 app.register_blueprint(chart_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000, host="0.0.0.0")
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True,
+    )
