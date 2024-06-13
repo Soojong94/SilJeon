ddd724a14322306f3fea359e495749f113eab672
minjae <sapoon11@gmail.com>
2024-06-13 11:26:40 +0900

    디비연결수늘림

--- your_voice/backend/db/db.py
+++ your_voice/backend/db/db.py
@@ -4,9 +4,9 @@ import pymysql
 # 커넥션 풀 설정
 pool = PooledDB(
     creator=pymysql,  # 사용할 DBMS
-    maxconnections=10,  # 최대 커넥션 수
-    mincached=2,  # 초기에 생성할 커넥션 수
-    maxcached=6,  # 최대로 유지할 커넥션 수
+    maxconnections=200,  # 최대 커넥션 수
+    mincached=10,  # 초기에 생성할 커넥션 수
+    maxcached=50,  # 최대로 유지할 커넥션 수
     maxusage=None,  # 한 커넥션에 최대 사용 횟수
     blocking=True,  # 커넥션 수가 부족할 때 블로킹할지 여부
     host="project-db-cgi.smhrd.com",
